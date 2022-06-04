ece5ae5754a9f123af108a7cfdcb879b68ad9925
wojciechmikolajczyk6 <wojciechmikolajczyk6@gmail.com>
2022-06-04 16:09:19 +0200

    init

--- resources/views/room/index.blade.php
+++ resources/views/room/index.blade.php
@@ -46,7 +46,7 @@
                             @foreach ($data as $room)
                         <tr>
                             <td>{{$room->id}}</td>
-{{--                            <td>{{$room->RoomType->title}}</td>--}}
+                            <td>{{$room->RoomType->title}}</td>
                             <td>{{$room->title}}</td>
                             <td>
                                 <a href="{{'/admin/room/'.$room->id}}" class="btn btn-info btn-sm"><i class="fa fa-eye"></i></a>
